2dca8fb543580a718ef3221c457d74915e1c1b1b
dangar0090 <dangarpriyank1@gmail.com>
2024-05-15 11:37:21 +0530

    removed boto3

--- main.py
+++ main.py
@@ -50,12 +50,7 @@ except Exception as e:
     logger.error("Error setting up environment variables: {}".format(str(e)))
     raise e
 #AWS S3 Configuration    
-s3 = boto3.client(
-    's3',
-    region_name='us-east-1',
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
-)
+region_name = os.getenv('REGION_NAME')
 bucket_name = os.getenv('S3_BUCKET_NAME')
 # Database configuration
 conn = psycopg2.connect(
